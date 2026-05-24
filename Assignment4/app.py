523555ad3c5d784cfd17016f5c56b1e279370beb
yeshey lhendup <lhendupyeshey918@gmail.com>
2026-05-24 15:31:22 +0600

    dso4

--- Assignment4/app.py
+++ Assignment4/app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,4 +6,4 @@ def home():
     return "Hello DevOps!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
